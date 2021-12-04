db2497c40d8815436cbd70da02d723107ae78f28
Thi Le <helenle8020@gmail.com>
2021-12-03 22:37:13 -0500

    create model accounts and design HTML files for account model

---
+++ accounts/views.py
@@ -0,0 +1,37 @@
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('homepage')

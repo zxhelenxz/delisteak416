ec5026fcc90cf7f8cd703d6acc79fa201eec26a5
Thi Le <helenle8020@gmail.com>
2021-12-08 19:42:26 -0500

    verson on Wednesday

--- reservations/forms.py
+++ reservations/forms.py
@@ -5,4 +5,4 @@ from .models import Reserve
 class ReserveForm(forms.ModelForm):
     class Meta:
         model = Reserve
-        fields = '__all__'
+        fields = ['time', 'date', 'person', 'table']
